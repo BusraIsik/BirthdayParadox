a39f1aabbb736680b2c2fdd01e881b30839a1e8b
Busra Isik <b.isikbusra@gmail.com>
2022-02-09 18:42:02 +0300

    updated code

--- BirthdayParadox/Program.cs
+++ BirthdayParadox/Program.cs
@@ -4,51 +4,53 @@ namespace BirthdayParadox
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Random generator = new Random();
-            int[,] matris = new int[5,15];
-            String[] hafta = { "Pazatesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar" };
+            int[,] matrix = new int[5,15];
+            string[] week = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
             for (int i = 0; i < 5; i++)
-            { //kişi sayısı
-                int[] kisi_sayisi = { 2, 3, 5, 10, 20 };
-                Console.WriteLine();
-                Console.WriteLine(kisi_sayisi[i] + "kişiik grup için veriler:");
+            { // for number of people
+                int[] peoples = { 2, 3, 5, 10, 20 };
+                if (i != 0) {
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Data for group of " + peoples[i] + ":");
+                Console.WriteLine("The number of overlaps in the days of the week:");
                 for (int j = 0; j < 15; j++)
-                { //deney sayısı
-                    int[] haftanin_gunleri = { 0, 0, 0, 0, 0, 0, 0 };
-                    Console.WriteLine(j + 1 + ". deney");
-                    int toplam = 0;
-                    for (int k = 0; k < kisi_sayisi[i]; k++)
+                { // for number of experiment
+                    int[] weekdays = { 0, 0, 0, 0, 0, 0, 0 };
+                    Console.WriteLine(j + 1 + ".experiment:");
+                    int total = 0;
+                    for (int k = 0; k < peoples[i]; k++)
                     {
-                        int sayi = generator.Next(7); //o ile 6 arasında değer üretecek
-                        haftanin_gunleri[sayi] += 1;
+                        int number = generator.Next(7); // create number of 0-6
+                        weekdays[number] += 1;
                     }
-                    Console.WriteLine("haftanın günlerindeki çakışma sayıları:");
                     int a = 0;
-                    foreach (int element in haftanin_gunleri)
-                    { // çakışma sayısını hesaplar
+                    foreach (int element in weekdays)
+                    { // calculate the number of overlaps
                         if (element == 0)
                         {
-                            Console.WriteLine(hafta[a] + ":" + element + " çakışma vardır");
+                            Console.WriteLine(week[a] + ":" + element + " overlap");
                         }
                         else
                         {
-                            toplam += element - 1;
-                            Console.WriteLine(hafta[a] + ":" + (element - 1) + " çakışma vardır");
+                            total += element - 1;
+                            Console.WriteLine(week[a] + ":" + (element - 1) + " overlap");
                         }
                         a += 1;
                     }
-                    matris[i,j] = toplam;
+                    matrix[i,j] = total;
                 }
             }
-            Console.WriteLine("15 deneyde ve 5 kişide toplam çakışma sayıları");
+            Console.WriteLine("\nTotal number of overlaps in 15 experiment and 5 different people numnber:");
             for (int b = 0; b < 5; b++)
             {
-                Console.WriteLine((b + 1) + ".kişi için:");
+                Console.WriteLine((b + 1) + ".people:");
                 for (int y = 0; y < 15; y++)
                 {
-                    Console.WriteLine(matris[b,y] + " ");
+                    Console.WriteLine(matrix[b,y] + " ");
                 }
                 Console.WriteLine();
             }
